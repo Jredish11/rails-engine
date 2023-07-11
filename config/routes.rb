741ab237e39c1cf2a685385fa44dd3f44c3cea20
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-11 15:55:16 -0600

    add routes for get request for merchant by id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
+      resources :merchants, only: [:index, :show]
     end
   end
   namespace :api do
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       resources :items, only: [:index]
     end
   end
+
   
   # Defines the root path route ("/")
   # root "articles#index"
