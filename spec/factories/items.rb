3bab22827ea99716e1ecb99c333abb6336ca31c6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-11 15:30:14 -0600

    fix merchant_id creation

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.sentence }
     unit_price { Faker::Commerce.price(range: 0..100.0) }
+    merchant_id { FactoryBot.create(:merchant) }
   end
 end
