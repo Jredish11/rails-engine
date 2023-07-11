6e4ff750fbcaee88cbc3fe02feecb1f80fe937b1
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-11 15:31:17 -0600

    add serializers, gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index]
     end
   end
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index]
+    end
+  end
   
   # Defines the root path route ("/")
   # root "articles#index"
