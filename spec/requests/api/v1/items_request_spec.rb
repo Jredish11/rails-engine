3982765df1b79a4563e397bc86e3c1525f4a6ade
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-12 08:50:06 -0600

    add tests for get one item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -37,7 +37,41 @@ RSpec.describe "Items API" do
 
     describe "GET /api/v1/items/{{item_id}}" do
       it "can get one item by its id" do
+        merchant = create(:merchant)
+        item_id = create(:item, merchant_id: merchant.id)
+        
+        get "/api/v1/items/#{item_id.id}"
 
+        expect(response).to be_successful
+
+        item = JSON.parse(response.body, symbolize_names: true)
+
+        expect(item).to have_key(:data)
+        expect(item[:data]).to be_a(Hash)
+
+        expect(item[:data]).to have_key(:id)
+        expect(item[:data][:id]).to be_a(String)
+
+        expect(item[:data]).to have_key(:type)
+        expect(item[:data][:type]).to be_a(String)
+        
+        expect(item[:data]).to have_key(:attributes)
+        expect(item[:data][:attributes]).to be_a(Hash)
+
+        expect(item[:data][:attributes]).to have_key(:name)
+        expect(item[:data][:attributes][:name]).to be_a(String)
+        
+        expect(item[:data][:attributes]).to have_key(:description)
+        expect(item[:data][:attributes][:description]).to be_a(String)
+
+
+        expect(item[:data][:attributes]).to have_key(:unit_price)
+        expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+
+
+        expect(item[:data][:attributes]).to have_key(:merchant_id)
+        expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+        expect(item[:data][:attributes][:merchant_id]).to eq(merchant.id)
       end
     end
   end
