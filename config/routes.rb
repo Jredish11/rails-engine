b40a72e015cae6ae93267017c092687fc95e5421
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-12 08:39:02 -0600

    add route for for show action

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index]
+      resources :items, only: [:index, :show]
     end
   end
 
