346902d7e2be98a86d40569b869da92cc301aa69
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-13 16:26:31 -0600

    add find_all action and query

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.all) 
+    render(json: MerchantSerializer.new(Merchant.all)) 
   end
 
   def show
-    render json: MerchantSerializer.new(Merchant.find(params[:id]))  
+    render(json: MerchantSerializer.new(Merchant.find(params[:id])))  
+  end
+
+  def find_all
+    merchants = Merchant.where("name ILIKE ?", "%#{params[:name]}%")
+    render(json: MerchantSerializer.new(merchants))
   end
 end
